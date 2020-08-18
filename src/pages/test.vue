87c6652c4279d3cbd5f61d17262786a9c5955c52
lengqing66 <524872828@qq.com>
2020-08-18 17:23:31 +0800

    0818

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,101 +1,62 @@
 <template>
-    <div id="content" class="p-2">
-        <form action="">
-            <div class="form-row" >
-                <div class="form-group col-md-6">
-                    <div class="row">
-                        <div class="col-md-3"><label class="col-form-label">text :</label></div>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" value="text" v-model="input1" onchange=""/>
-                        </div>
-                    </div>
-                </div>
-                <!--<div class="form-group col-md-6">-->
-                    <!--<div class="row">-->
-                        <!--<div class="col-md-3"><label class="col-form-label">select :</label></div>-->
-                        <!--<div class="col-md-9">-->
-                            <!--<select v-model="test.input2">-->
-                                <!--<option value ="test1" class="form-control">test1</option>-->
-                                <!--<option value ="test2" class="form-control">test2</option>-->
-                            <!--</select>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-                <!--<div class="form-group col-md-6">-->
-                    <!--<div class="row">-->
-                        <!--<div class="col-md-3"><label class="col-form-label">textarea :</label></div>-->
-                        <!--<div class="col-md-9">-->
-                            <!--<textarea rows="3" cols="20" class="form-control" v-model="test.input3">-->
-                                <!--test_textarea-->
-                            <!--</textarea>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-                <!--<div class="form-group col-md-6">-->
-                    <!--<div class="row">-->
-                        <!--<div class="col-md-3"><label class="col-form-label">radio :</label></div>-->
-                        <!--<div class="col-md-9">-->
-                            <!--<label><input type="radio"/>radio1 </label>-->
-                            <!--<label><input type="radio"/>radio2 </label>-->
-                        <!--</div>-->
-                    <!--</div>-->
-                <!--</div>-->
-                <div class="form-group col-md-6">
-                    <div class="row">
-                        <button type="submit" @click.prevent="onSubmit()"><span>保存</span></button>
-                    </div>
-                </div>
-            </div>
+        <form action="" name="form1test">
+
+            <label class="col-form-label">text :</label>
+            <input type="text" class="form-control" @change="Test(test.input1,$event)" :value="test.input1" name="text"/>
+
+            <label class="col-form-label">select :</label>
+            <select class="form-control" :value="test.input2" @change="Test(test.input2,$event)" name="select">
+                <option value ="test1" selected>test1</option>
+                <option value ="test2">test2</option>
+            </select>
+
+            <label class="col-form-label">select :</label>
+            <textarea class="form-control" @change="Test(test.input3,$event)" :value="test.input3" name="textarea"/>
+
+            <input type="hidden" v-model="hidden_test" id="hidden"/>
+
+            <button type="submit" @click.prevent="onSubmit()"><span>提交</span></button>
+
         </form>
-    </div>
 </template>
 
 <script>
-    import axios from 'axios';
 
     export default {
         name: "test",
+        changeLog:[],
         data() {
             return {
-                // test:{
-                    input1:'',
-                //     "input2":'',
-                //     "input3":'',
-                // },
-                inputData:[]
+                test:
+                    {
+                    "input1":'test',
+                    "input2":'test1',
+                    "input3":'textarea',
+                },
+                array:[],
+                hidden_test:[],
             }
         },
         methods: {
+            Test(test,event) {
+                if (test !== event) {
+                    this.changehidden(test, event.target.value,event.target.name);
+                }
+            },
+            changehidden(oldValue,newValue,name){
+                let json_obj={};
+                json_obj.name=name;
+                json_obj.oldValue=oldValue;
+                json_obj.newValue=newValue;
+                this.array=JSON.stringify(json_obj);
+                this.hidden_test.push(this.array);
+                console.log(this.hidden_test);
+            },
             onSubmit() {
-
-                // let formData=new FormData();
-                // for(var key in this.test){
-                //     formData.append(key,this.test[key]);
-                // }
-                // axios({
-                //     method:"post",
-                //     url:"",
-                //     headers:{
-                //         "Content-Type":"application/json"
-                //     },
-                //     withCredentials:true,
-                //     data:formData
-                // }).then((res)=>{
-                //     console.log(123);
-                //     console.log(res);
-                // });
             }
         }
     }
-    axios({
-        method:"post",
-        url:"",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        withCredentials:true,
-        data:this.input1
-    })
+
 </script>
 
 <style scoped>
