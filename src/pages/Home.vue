7302642fc24000ef2d6fb934aa2e2f59485b27c0
lengqing66 <524872828@qq.com>
2020-08-27 09:31:35 +0800

    0827

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,20 @@
 <template>
     <div id="content" class="app-content">
         <!--content-->
-        <!--<hiddenChange></hiddenChange>-->
-        <ASelect></ASelect>
+        <hiddenChange></hiddenChange>
+        <!--<ASelect></ASelect>-->
     </div>
 
 </template>
 
 <script>
-    // import hiddenChange from '../layouts/components/hiddenChange';
-    import ASelect from '../layouts/components/ASelect';
+    import hiddenChange from '../layouts/components/hiddenChange';
+    // import ASelect from '../layouts/components/ASelect';
 
     export default {
         components: {
-            // hiddenChange,
-            ASelect,
+            hiddenChange,
+            // ASelect,
         }
     }
 </script>
